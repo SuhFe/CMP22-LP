48a53f26ed76951a70f29b1a0183dcaf0539feb5
Suzana <su_zana-santos@hotmail.com>
2022-05-30 19:00:07 -0300

    Acrescentando exercicio javascript.

--- variaveis.js
+++ variaveis.js
@@ -1,28 +1,29 @@
-/*
-  Existem 2 formas de declarar variáveis no JS.
-  var: não recomendado o uso, não respeita escopos (Hoisting).
-  let: respeita o escopo e é recomendado o uso.
-*/
-
 // Nome do pais
 // Pontos turisticos
 // Capital do país
 // Quantidade de estados
 // O país é da América Latina
 
-/* 
-  Declarando constante em JS
-  const: o valor de um dado imultável
-*/
+//const nomePais = "Brasil";
+//let pontosTuristicos = "Cristo redendor, lencois";
+//const capital = "Brasilia";
+//const numeroEstados = "27";
+//const americaLatina = "Sim";
 
-const nomePais = "Brasil";
-let pontosTuristicos = "Cristo redendor";
-const capital = "Brasilia";
-const numeroEstados = "27"; //declarando numero de estados
-const americaLatina = "Sim";
+//console.log(nomePais);
+//console.log(pontosTuristicos);
+//console.log(capital);
+//console.log(numeroEstados);
+//console.log(americaLatina);
 
-console.log(nomePais);
-console.log(pontosTuristicos);
-console.log(capital);
-console.log(numeroEstados);
-console.log(americaLatina);
+
+const nome = 'Suzana';
+let idade = '29';
+let altura = '1.55';
+let temFilho = 'Sim';
+
+
+console.log('Meu nome é',nome);
+console.log('Tenho', idade, 'anos');
+console.log(altura, 'de altura');
+console.log('E se tenho filhos?', temFilho, ',tenho.');
