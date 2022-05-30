b80d4af57c3017ae7721739a13341838e9c33db8
Suzana <su_zana-santos@hotmail.com>
2022-05-30 17:21:35 -0300

    acrescentando comentários no JS

---
+++ variaveis.js
@@ -0,0 +1,28 @@
+/*
+  Existem 2 formas de declarar variáveis no JS.
+  var: não recomendado o uso, não respeita escopos (Hoisting).
+  let: respeita o escopo e é recomendado o uso.
+*/
+
+// Nome do pais
+// Pontos turisticos
+// Capital do país
+// Quantidade de estados
+// O país é da América Latina
+
+/* 
+  Declarando constante em JS
+  const: o valor de um dado imultável
+*/
+
+const nomePais = "Brasil";
+let pontosTuristicos = "Cristo redendor";
+const capital = "Brasilia";
+const numeroEstados = "27"; //declarando numero de estados
+const americaLatina = "Sim";
+
+console.log(nomePais);
+console.log(pontosTuristicos);
+console.log(capital);
+console.log(numeroEstados);
+console.log(americaLatina);
