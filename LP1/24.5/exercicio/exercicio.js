cc1e95c9f815ea379ea28d86bd5b4e05bc3800af
Suzana <su_zana-santos@hotmail.com>
2022-06-24 00:58:12 -0300

    reorganizando pastas

---
+++ LP1/24.5/exercicio/exercicio.js
@@ -0,0 +1,29 @@
+// Nome do pais
+// Pontos turisticos
+// Capital do país
+// Quantidade de estados
+// O país é da América Latina
+
+const nomePais = "Brasil";
+let pontosTuristicos = "Cristo redendor, lencois";
+const capital = "Brasilia";
+const numeroEstados = "27";
+const americaLatina = "Sim";
+
+//console.log(nomePais);
+//console.log(pontosTuristicos);
+//console.log(capital);
+//console.log(numeroEstados);
+//console.log(americaLatina);
+
+
+const nome = 'Suzana';
+let idade = '29';
+let altura = '1.55';
+let temFilho = 'Sim';
+
+
+//console.log('Meu nome é',nome);
+//console.log('Tenho', idade, 'anos');
+//console.log(altura, 'de altura');
+//console.log('E se tenho filhos?', temFilho, ',tenho.');
