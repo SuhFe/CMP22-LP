5eb69fdf155cbc4f120b12beb24d901001b63073
Suzana <su_zana-santos@hotmail.com>
2022-06-24 20:49:28 -0300

    adicionando exercicios

---
+++ LP1/24.5/aula/variaveis1.js
@@ -0,0 +1,13 @@
+console.log("olá mundo, cheguei para dominar programação!")
+
+const pais = "Brasil";
+let pontos_turisticos = "Pão de açucar";
+const capital = "Brasilia";
+const estados = "27";
+
+
+
+console.log(pais);
+console.log(pontos_turisticos);
+console.log(capital);
+console.log(estados);
---
+++ LP1/24.5/exercicio/exercicio-coletivo.js
@@ -0,0 +1,13 @@
+const nomePais = "Brasil";
+let pontosTuristicos = "Pão de açucar";
+const capital = "Brasilia";
+const numeroEstados = 27;
+const fazParteAmericaLatina = "sim";
+
+
+
+console.log(nomePais);
+console.log(pontosTuristicos);
+console.log(capital);
+console.log(numeroEstados);
+console.log(fazParteAmericaLatina);
