58f421e88ebf53a3bc9e33722f6a01398077242d
Suzana <su_zana-santos@hotmail.com>
2022-06-24 19:22:32 -0300

    reorganizando pastas

---
+++ LP1/26.5/aula/index.js
@@ -0,0 +1,21 @@
+/*
+  Concatenar é o ato de juntar textos 
+  e/ou variáveis em uma unica string.
+*/
+const primeiroNome = "Suzana";
+const ultimoNome = "Araujo";
+const idade = 29;
+
+console.log(
+  `Meu nome é ${primeiroNome} ${ultimoNome} e eu tenho ${idade} anos`
+);
+
+console.log(
+  "Meu nome é " +
+    primeiroNome +
+    " " +
+    ultimoNome +
+    " e eu tenho " +
+    idade +
+    " anos"
+);
---
+++ LP2/02.6/aula/condicional_simples.js
@@ -0,0 +1,6 @@
+//Condicional simples 
+let estadoNascimento = 'Pernambuco';
+
+if(estadoNascimento === "Pernambuco") {
+    console.log("é uma pessoa pernambucana! ");
+}
