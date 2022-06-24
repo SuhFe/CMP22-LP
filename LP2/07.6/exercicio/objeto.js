d3c0c0be97215277c37f21287e50ca670e08bd3f
Suzana <su_zana-santos@hotmail.com>
2022-06-24 19:39:10 -0300

    reorganizando pastas

---
+++ LP2/07.6/exercicio/objeto.js
@@ -0,0 +1,14 @@
+const aluna = { nome: "Suzana Araujo", idade: "29 anos" };
+const professora = {
+  nome: "Lisandra Cruz",
+  cargo: "Professora",
+  empresa: "Minas Programam",
+  signo: "Leão",
+};
+const disciplinaLP = ["LP1", "LP2", "LP3"];
+
+/* concatene todas as informações presentes em professora, 
+aluna e disciplinaLP em um único parágrafo/frase */
+console.log(
+  `A aluna ${aluna.nome} está estudando as disciplinas ${disciplinaLP[0]}, ${disciplinaLP[1]} e ${disciplinaLP[2]} com a professora ${professora.nome} no ${professora.empresa}`
+);
